9d596d251290a884dd3eb0638961bb64f719ac90
Theodore Moreland <theodoremoreland@yahoo.com>
2021-06-27 10:38:32 -0500

    Completed search jobs functionality. App now displays jobs upon applying filter in search section.

--- src/main/java/org/launchcode/TechJobsMVC/controllers/SearchController.java
+++ src/main/java/org/launchcode/TechJobsMVC/controllers/SearchController.java
@@ -1,8 +1,12 @@
 package org.launchcode.TechJobsMVC.controllers;
 
+import org.launchcode.TechJobsMVC.models.Job;
+import org.launchcode.TechJobsMVC.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 import static org.launchcode.TechJobsMVC.controllers.ListController.columnChoices;
 
@@ -19,6 +23,21 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+
+        if (searchTerm.equals("all") || searchTerm.equals("")) {
+            jobs = JobData.findAll();
+        }
+        else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 
 }
